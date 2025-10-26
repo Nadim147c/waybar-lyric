c4eb7c018a309d310b9aadc824b3352e6e2851e8
Ephemeral <theephemeral.txt@gmail.com>
2025-10-26 21:49:03 +0600

    fix: inject version from git tag on build

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"io"
 	"os"
 	"syscall"
@@ -12,13 +13,19 @@ import (
 	"github.com/Nadim147c/waybar-lyric/internal/config"
 )
 
+var Version = "dev-build"
+
+var version = fmt.Sprintf("waybar-lyric %s (https://github.com/Nadim147c/waybar-lyric)", Version)
+
 func main() {
+	config.Version = Version
+
 	err := fang.Execute(
 		context.Background(),
 		cmd.Command,
 		fang.WithFlagTypes(),
 		fang.WithNotifySignal(os.Interrupt, os.Kill, syscall.SIGKILL, syscall.SIGTERM),
-		fang.WithVersion(config.Version),
+		fang.WithVersion(version),
 		fang.WithoutCompletions(),
 		fang.WithoutManpage(),
 		fang.WithErrorHandler(func(w io.Writer, styles fang.Styles, err error) {
