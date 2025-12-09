697f30f671a3a7671e6a795b55b7bc8a827dd0c3
Ephemeral <theephemeral.txt@gmail.com>
2025-12-09 11:20:55 +0600

    chore: add nix flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem = {pkgs, ...}: {
+        packages.default = pkgs.callPackage ./nix/package.nix {};
+        devShells.default = pkgs.callPackage ./nix/shell.nix {};
+      };
+    };
+}
