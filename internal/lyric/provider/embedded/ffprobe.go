27baff925deecf53bea747e870dbe68908c799e7
Ephemeral <ephemeral.those316@slmails.com>
2026-04-28 12:33:42 +0600

    feat: add support for loading embedded lyrics

---
+++ internal/lyric/provider/embedded/ffprobe.go
@@ -0,0 +1,74 @@
+package embedded
+
+import (
+	"context"
+	"encoding/json"
+	"maps"
+	"os/exec"
+
+	"github.com/Nadim147c/waybar-lyric/internal/lyric/models"
+	"github.com/Nadim147c/waybar-lyric/internal/lyric/provider"
+	"github.com/Nadim147c/waybar-lyric/internal/player"
+)
+
+type ffprobeOutput struct {
+	Streams []streams `json:"streams"`
+}
+type streams struct {
+	Tags map[string]string `json:"tags"`
+}
+
+// Provider is a lyrics provider that gets lyrics from `LYRICS` tags of local
+// file.
+var Provider = provider.NewProvider("embedded lyrics in audio file",
+	func(ctx context.Context, metadata *player.Metadata) (lyrics models.Lyrics, err error) {
+		lyrics.Metadata = metadata
+		lyrics.NoCache = true
+
+		if metadata.URL.Scheme != "file" {
+			return lyrics, models.ErrLyricsNotFound
+		}
+
+		ffprobe, err := exec.LookPath("ffprobe")
+		if err != nil {
+			return lyrics, err
+		}
+
+		path := metadata.URL.Path
+		output, err := exec.CommandContext(ctx, ffprobe,
+			"-v", "quiet",
+			"-show_streams",
+			"-print_format", "json",
+			path,
+		).Output()
+		if err != nil {
+			return lyrics, err
+		}
+
+		var result ffprobeOutput
+		err = json.Unmarshal(output, &result)
+		if err != nil {
+			return lyrics, err
+		}
+
+		tags := map[string]string{}
+		for _, stream := range result.Streams {
+			maps.Copy(tags, stream.Tags)
+		}
+
+		keys := []string{"LYRICS", "SYLT", "USLT", "lyrics", "lyrics-eng"}
+		for _, key := range keys {
+			value, ok := tags[key]
+			if !ok || value == "" {
+				continue
+			}
+			lines, err := provider.ParseText(value)
+			if err != nil {
+				continue
+			}
+			lyrics.Lines = lines
+			return lyrics, nil
+		}
+
+		return lyrics, models.ErrLyricsNotFound
+	})
